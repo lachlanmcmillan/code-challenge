0db45fd286299dbb7aa036b3ddb88169bec7aede
Lachlan McMillan <lachlanmcmillan@users.noreply.github.com>
2024-04-15 11:19:04 +1000

    fix header rounded corners

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -7,22 +7,24 @@ const Header = () => {
   }
   return (
     <header>
-      <nav>
-        <Link className="brand" href="/">
-          <img src="/brand.svg" alt="birchal" />
-        </Link>
-        <Link href="/raise">Raise Capital</Link>
-        <Link href="/investors">Investors</Link>
-        <Link href="/newsfeed">Newsfeed</Link>
-        <Link href="/legal">Legal Centre</Link>
-        <Link href="/contact">Contact</Link>
-        <button className="outline" type="button" onClick={browseCompanies}>
-          Browse Companies
-        </button>
-        <button className="primary" type="button">
-          Sign In
-        </button>
-      </nav>
+      <div className="inner">
+        <nav>
+          <Link className="brand" href="/">
+            <img src="/brand.svg" alt="birchal" />
+          </Link>
+          <Link href="/raise">Raise Capital</Link>
+          <Link href="/investors">Investors</Link>
+          <Link href="/newsfeed">Newsfeed</Link>
+          <Link href="/legal">Legal Centre</Link>
+          <Link href="/contact">Contact</Link>
+          <button className="outline" type="button" onClick={browseCompanies}>
+            Browse Companies
+          </button>
+          <button className="primary" type="button">
+            Sign In
+          </button>
+        </nav>
+      </div>
     </header>
   );
 };
